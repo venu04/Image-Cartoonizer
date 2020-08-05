e10bc76c86215a48768ac7f6d38c1c9baa3f34c5
venugopal <43407734+venu04@users.noreply.github.com>
2020-08-05 21:22:57 +0530

    commiting first upload

---
+++ app.js
@@ -0,0 +1,97 @@
+const express=require('express')
+const multer=require('multer')
+const ejs=require('ejs')
+const path=require('path')
+
+// Set Storage Engine
+const storage=multer.diskStorage({
+    destination:'./public/uploads/',
+    filename: function(req,file,cb){
+        cb(null,file.fieldname + '-'+Date.now()+path.extname(file.originalname))
+    }
+})
+
+//Init Upload
+const upload =multer({
+    storage:storage,
+    //limits:{fileSize:10},
+    fileFilter:function(req,file,cb){
+        checkFileType(file,cb)
+    }
+}).single('myimage')
+
+//check file type
+function checkFileType(file,cb){
+    // Allowed ext
+    const filetypes=/jpeg|jpg|png|gif/
+    //check ext
+    const extname=filetypes.test(path.extname(file.originalname).toLowerCase())
+    //check mime
+    const mimetype=filetypes.test(file.mimetype)
+
+    if(mimetype && extname){
+        return cb(null,true)
+    }
+    else{
+        cb("Error Images only")
+    }
+}
+
+//Init app
+const app=express()
+
+//Ejs
+app.set('view engine','ejs')
+
+//public folder
+app.use(express.static('./public'))
+
+app.get('/',function(req,res){
+    res.render('index')
+})
+
+
+
+app.post('/upload',function(req,res){
+    upload(req,res,function(err){
+        if(err){
+            res.render('index',{
+                msg:err
+            })
+        }
+        else{
+            if(req.file==undefined){
+                res.render('index',{
+                    msg:'Error: No File Selected'
+                })
+            }
+            else{
+                
+                console.log('callname '+req.file.filename)
+                var spawn=require('child_process').spawn
+            
+                var process=spawn('python',["./cartoonize.py",req.file.filename])
+                process.stdout.on('data',function(data){
+                    console.log('output after '+data)
+
+                    res.render('index',{
+                        msg:'File Uploaded!',
+                        file:`uploads/${req.file.filename}`,
+                        file4:`pyimages/${req.file.filename}`
+                    })
+
+
+                })
+            }
+        }
+    })
+})
+
+
+
+
+const port = 3000;
+
+app.listen(port,function(){
+    console.log(`server started on port ${port}`)
+})
